f1ed3ec0746e312b7ff784b30db8a9bf8122d9a5
Kaream <kareamtanany@gmail.com>
2025-02-11 15:42:13 +0200

    create featyred list view

--- lib/Features/home/presentation/view/widgets/home_view_body.dart
+++ lib/Features/home/presentation/view/widgets/home_view_body.dart
@@ -10,10 +10,28 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
 }
 
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .25,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: CustomListViewItem(),
+          );
+        },
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+}
