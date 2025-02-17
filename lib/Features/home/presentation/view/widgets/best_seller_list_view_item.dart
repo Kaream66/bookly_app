4c9265696855a0129e40b4d74b656c4c7e9f4fcf
Kaream <kareamtanany@gmail.com>
2025-02-17 14:28:09 +0200

    create best seller list view item

---
+++ lib/Features/home/presentation/view/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,35 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              height: MediaQuery.of(context).size.height * .25,
+              width: MediaQuery.of(context).size.width * .2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(),
+        ],
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/view/widgets/home_view_body.dart
+++ lib/Features/home/presentation/view/widgets/home_view_body.dart
@@ -1,7 +1,10 @@
+import 'package:bookly_app/Features/home/presentation/view/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/view/widgets/featured_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,8 +12,8 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 20),
-      child:  Column(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
@@ -21,9 +24,12 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.titleMedium,
-          )
+          ),
+          SizedBox(height: 20,),
+          BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
+
