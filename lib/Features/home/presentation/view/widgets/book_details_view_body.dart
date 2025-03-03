665c429b0a392dbb6a31143dab033da5f3618264
Kaream <kareamtanany@gmail.com>
2025-03-03 13:28:43 +0200

    add custom bbok image to book details body

--- lib/Features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/home/presentation/view/widgets/book_details_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/view/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,14 +7,19 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 30.0),
+    var width = MediaQuery.of(context).size.width;
+
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         children: [
-          CustomBokkDetailsAppBar(),
+         const CustomBokkDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const CustomBookImage(),
+          ),
         ],
       ),
     );
   }
 }
-
